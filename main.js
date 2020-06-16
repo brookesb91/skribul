0cca00de98de30cbd402063c605de21a0ab92163
Ben Brookes <brookesb91@hotmail.com>
2020-06-16 22:07:22 +0100

    :rocket: Init

---
+++ main.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const randomstring = require('randomstring');
+const app = express();
+
+const protocol = process.env.PROTOCOL || 'http';
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+const dbUri = 'mongodb://localhost/skribul';
+
+
+const saveSchema = new mongoose.Schema({
+  image: Buffer,
+  slug: {
+    type: String,
+    default: function () {
+      return randomstring.generate(8);
+    }
+  }
+});
+
+saveSchema.methods.toJSON = function () {
+  return {
+    image: this.image.toString(),
+    slug: this.slug
+  };
+}
+
+const Save = mongoose.model('Save', saveSchema);
+
+app.set('view engine', 'pug');
+
+app.use(express.json({
+  limit: '50mb',
+  parameterLimit: 50000
+}));
+app.use(express.urlencoded({
+  limit: '50mb'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/:slug', async (req, res) => {
+  const slug = req.params['slug'];
+
+  const save = await Save.findOne({
+    slug
+  });
+
+  if (!save) {
+    // throw
+    console.log('Not Found');
+  }
+
+  return res.render('view', save.toJSON());
+})
+
+app.put('/api/saves', async (req, res) => {
+  const {
+    image
+  } = req.body;
+
+  const item = await Save.create({
+    image: new Buffer(new Buffer(image, 'binary').toString('base64'), 'base64')
+  });
+
+  return res.json(item.toJSON());
+});
+
+app.get('/api/saves/:slug', async (req, res) => {
+  const slug = req.params['slug'];
+
+  const save = await Save.findOne({
+    slug
+  });
+
+  if (!save) {
+    // throw
+  }
+
+  return res.json(save.toJSON());
+});
+
+const server = new http.Server(app);
+
+server.listen(port, host, () => {
+
+  mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+
+  console.log(`Server running on ${protocol}://${host}:${port}`);
+});
---
+++ public/scripts/skribul.js
@@ -0,0 +1,104 @@
+const getCanvas = () => document.getElementById('canvas');
+const getContext = () => getCanvas().getContext('2d');
+const getRect = () => getCanvas().getBoundingClientRect();
+
+const isTouch = () => 'ontouchstart' in window;
+
+getCanvas().width = document.body.clientWidth;
+getCanvas().height = document.body.clientHeight;
+
+const TOUCH_EVENTS = {
+  move: 'touchmove',
+  start: 'touchstart',
+  end: 'touchend'
+};
+
+const MOUSE_EVENTS = {
+  move: 'mousemove',
+  start: 'mousedown',
+  end: 'mouseup'
+};
+
+const getInputEventNames = () => isTouch() ? TOUCH_EVENTS : MOUSE_EVENTS;
+
+const INPUT_MOVE = getInputEventNames().move;
+const INPUT_START = getInputEventNames().start;
+const INPUT_END = getInputEventNames().end;
+
+const setStyle = (color) => getContext().fillStyle = color;
+
+const clearCanvas = () => {
+  const {
+    width,
+    height
+  } = getCanvas();
+  getContext().clearRect(0, 0, width, height);
+};
+
+const drawStart = (e) => getCanvas().addEventListener(INPUT_MOVE, startPath);
+
+const drawEnd = (e) => getCanvas().removeEventListener(INPUT_MOVE, startPath);
+
+const startPath = (e) => {
+  const pos = getInputPos(e);
+  getContext().fillRect(pos.x - 1, pos.y - 1, 3, 3);
+};
+
+const getInputPos = (e) => isTouch() ? getTouchPos(e) : getMousePos(e);
+
+const getTouchPos = (e) => {
+  const {
+    left,
+    top
+  } = getRect();
+  return {
+    x: e.touches[0].clientX - left,
+    y: e.touches[0].clientY - top
+  }
+}
+
+const getMousePos = (e) => {
+  const {
+    left,
+    top
+  } = getRect();
+  return {
+    x: e.clientX - left,
+    y: e.clientY - top,
+  };
+};
+
+const render = (dataURI) => {
+  const image = new Image();
+  image.onload = () => getContext().drawImage(image, 0, 0);
+  image.src = dataURI;
+}
+
+const save = async () => {
+  const image = getCanvas().toDataURL('image/png');
+  const payload = {
+    image
+  };
+
+  const res = await fetch('api/saves', {
+    method: 'PUT',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  });
+
+  const data = await res.json();
+  window.location.href = `/${data.slug}`;
+
+  const link = window.location;
+
+  await navigator.clipboard.writeText(link);
+
+  alert(`Copied to clipboard`);
+}
+
+getCanvas().addEventListener(INPUT_START, drawStart);
+
+getCanvas().addEventListener(INPUT_END, drawEnd);
