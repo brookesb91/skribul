b7cba559c8cd4171530a439a27d8dd079db2145d
Ben Brookes <brookesb91@hotmail.com>
2020-06-19 18:03:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const forceSsl = function (req, res, next) {
+const forceSsl = (req, res, next) => {
   if (req.headers['x-forwarded-proto'] !== 'https') {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
