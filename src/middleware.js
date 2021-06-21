47f12507c04831f994debf6d95aaa10ffb60d4bb
brookes22 <brookesb91@hotmail.com>
2021-06-21 14:06:57 +0100

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 const forceSsl = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
+  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
   return next();
