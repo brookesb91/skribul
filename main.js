d41b3f632848e6e70c30982d39386a271e356786
Ben Brookes <brookesb91@hotmail.com>
2021-06-23 19:38:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,11 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(
   express.urlencoded({
+    limit: '50mb',
     extended: true,
   })
 );
